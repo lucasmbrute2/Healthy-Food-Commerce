8316c7fd9976cc2e405a1b19b8f0698a0a4c073e
Lucas <lucasmbrute614@gmail.com>
2021-10-25 16:34:14 -0300

    feat(Home): implement app structure and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import { Switch, Route } from "react-router-dom"
+import Home from './components/pages/Home/Home';
 function App() {
   return (
     <div className="App">
-      Hello
+      <Switch>
+        <Route path='/' exact={true} component={Home}/>
+      </Switch>
     </div>
   )
 }

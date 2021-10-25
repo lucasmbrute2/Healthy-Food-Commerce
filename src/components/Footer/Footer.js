38e0e9660e71e6f50f202593ad93a76122eb3caf
Lucas <lucasmbrute614@gmail.com>
2021-10-25 18:47:18 -0300

    feat(Footer): new component footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,18 @@
+import "./Footer.scss"
+
 export default function Footer(){
-    return <footer>Copyright</footer>
+    const copyrightSymbol = "\u00a9"
+    
+    return(
+        <footer>
+            <div>
+                <p>{copyrightSymbol} Copyrights 2019 Stack. All Rights Reserved. </p>
+            </div>
+            
+            <div>
+                <p>Privacy Policy </p>
+                <p>Terms and Conditions</p>
+            </div>
+        </footer>
+    ) 
 }

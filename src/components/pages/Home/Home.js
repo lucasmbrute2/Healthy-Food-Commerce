82191d45ac05b3ca75d17f24d27c383b16bbf19e
Lucas <lucasmbrute614@gmail.com>
2021-10-25 18:08:14 -0300

    feat(Service): new page Serive

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
+import BestRecipes from "../../BestRecipes/BestRecipes";
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
+import Services from "../../Services/Services";
+
 import "./Home.css"
 export default function Home(){
     return(
         <div className='home'>
             <Header/>
-            Home
+            <BestRecipes/>
+            <Services/>
             <Footer/>
         </div>
         

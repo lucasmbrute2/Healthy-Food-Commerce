da4203601983332fce8ef3d2b55fd10707acac38
Lucas <lucasmbrute614@gmail.com>
2021-10-26 02:16:13 -0300

    feat(Header):style in header nav list

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,23 @@
-import "./Header.css"
+import "./Header.scss"
 
 export default function Header(){
     return(
         <div className='header'>
-            <div>
-                Healthy Food
-            </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li>Healthy Recipes</li>
-                        <li>Blog</li>
-                        <li>Join</li>
-                        <li>Register</li>
-                    </ul>
-                </nav>
-            </div>
-            
+            <div className='header-container'>
+                <div className='header-container-logo'>
+                    <h1 className='header-container-logo-h1'>Healthy Food</h1>
+                </div>
+                <div className='header-container-list'>
+                    <nav className='header-container-list-nav'>
+                        <ul className='header-container-list-nav-ul'>
+                            <li>HEALTHY RECIPES</li>
+                            <li>BLOG</li>
+                            <li>JOIN</li>
+                            <li id='li-register'>REGISTER</li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>  
         </div>
     )
 }

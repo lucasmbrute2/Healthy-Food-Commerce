d2bb36f44d9695510668580dd82f4d38951b147e
Lucas <lucasmbrute614@gmail.com>
2021-10-26 15:41:03 -0300

    feat(BestRecipes): new page with style

--- src/components/BestRecipes/BestRecipes.js
+++ src/components/BestRecipes/BestRecipes.js
@@ -3,57 +3,57 @@ import food2 from "../../img/comida_2.svg"
 import food3 from "../../img/comida_3.svg"
 import food4 from "../../img/comida_4.svg"
 import "./BestRecipes.scss" 
+
 export default function BestRecipes(){
     return(
         <div className='bestRecipes'>
-            <div>
-                <h2>Our Best Recipes</h2>
-                <p>Far far away, behind the word mountains, far from the coutries</p>
-                <p>Vokalia and Consonantia, there live the blind texts.</p>
+            <div className='bestRecipes-title'>
+                <h2 className='bestRecipes-title-h2'>Our Best Recipes</h2>
+                <p className='bestRecipes-title-p'>Far far away, behind the word mountains, far from the coutries</p>
+                <p className='bestRecipes-title-p bestRecipes-title-p-bottom '>Vokalia and Consonantia, there live the blind texts.</p>
             </div>
-            <div>
-                <div>
-                    <div className='teste'>
+            <div className='bestRecipes-container'>
+                <div className='bestRecipes-container-div'>
+                    <div className='bestRecipes-container-div-img'>
                         <img src={food1} alt='Food' className='teste-img'/>
                     </div>
                     
-                    <div>
-                        <h2>Broccoli Saladwith Bacon</h2>
-                        <button>See Recipe</button>
+                    <div className='bestRecipes-container-div-h2'>
+                        <h2 className='bestRecipes-h2'>Broccoli Saladwith Bacon</h2>
+                        <button className='bestRecipes-button'>See Recipe</button>
                     </div>
                 </div>
                 
-                <div>
-                    <div>
+                <div className='bestRecipes-container-div'>
+                    <div className='bestRecipes-container-div-img'>
                         <img src={food2} alt='Food'/>
                     </div>
                     
-                    <div>
-                        <h2>Classic BeefBurgers</h2>
-                        <button>See Recipe</button>
+                    <div className='bestRecipes-container-div-h2'>
+                        <h2 className='bestRecipes-h2'>Classic BeefBurgers</h2>
+                        <button className='bestRecipes-button'>See Recipe</button>
                     </div>
                 </div>
                 
-                <div>
-                    <div>
+                <div className='bestRecipes-container-div'>
+                    <div className='bestRecipes-container-div-img'>
                         <img src={food3} alt='Food'/>
                     </div>
                     
-                    <div>
-                        <h2>Classic BeefBurgers</h2>
-                        <button>See Recipe</button>
+                    <div className='bestRecipes-container-div-h2'>
+                        <h2 className='bestRecipes-h2'>Classic Potato Salad</h2>
+                        <button className='bestRecipes-button'>See Recipe</button>
                     </div>
                 </div>
                 
-                <div>
-                    <div>
+                <div className='bestRecipes-container-div'>
+                    <div className='bestRecipes-container-div-img'>
                         <img src={food4} alt='Food'/>
                     </div>
                     
-                    <div>
-                        <h2>Cherry Cobbleron the Grill
-</h2>
-                        <button>See Recipe</button>
+                    <div className='bestRecipes-container-div-h2'>
+                        <h2 className='bestRecipes-h2'>Cherry Cobbleron the Grill</h2>
+                        <button className='bestRecipes-button'>See Recipe</button>
                     </div>
                 </div>
 

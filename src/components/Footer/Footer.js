cc27120ca75de0a2d28830fc4304f359a266dd31
Lucas <lucasmbrute614@gmail.com>
2021-10-26 16:39:32 -0300

    feat(footer): implement footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,16 @@ export default function Footer(){
     const copyrightSymbol = "\u00a9"
     
     return(
-        <footer>
-            <div>
-                <p>{copyrightSymbol} Copyrights 2019 Stack. All Rights Reserved. </p>
-            </div>
-            
-            <div>
-                <p>Privacy Policy </p>
-                <p>Terms and Conditions</p>
-            </div>
+        <footer className='footer'>
+                <div className='footer-container'>
+                    <p className='footer-container-p'>{copyrightSymbol} Copyrights 2019 Stack. All Rights Reserved. </p>
+                </div>
+                
+                <div className='footer-container'>
+                    <p className='footer-container-p footer-container-p-privacy'>Privacy Policy </p>
+                    <p className='footer-container-p footer-container-p-privacy'>Terms and Conditions</p>
+                </div>
+
         </footer>
     ) 
 }
